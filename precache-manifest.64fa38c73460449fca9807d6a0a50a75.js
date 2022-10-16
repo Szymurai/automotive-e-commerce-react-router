85e691acde80bdc8d1b76be33e51523ace2181a4
Szymurai <szymuray@gmail.com>
2022-10-16 11:57:25 +0200

    Updates

---
+++ precache-manifest.64fa38c73460449fca9807d6a0a50a75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8601b37df89a8a27ec3f",
+    "url": "/automotive-e-commerce-react-router/static/css/main.5806fc0c.chunk.css"
+  },
+  {
+    "revision": "8601b37df89a8a27ec3f",
+    "url": "/automotive-e-commerce-react-router/static/js/main.9d557b22.chunk.js"
+  },
+  {
+    "revision": "d56e1ee94eeca5a7ac69",
+    "url": "/automotive-e-commerce-react-router/static/js/runtime~main.b04fd88d.js"
+  },
+  {
+    "revision": "5a60931798cb23807b33",
+    "url": "/automotive-e-commerce-react-router/static/js/2.bc8ae775.chunk.js"
+  },
+  {
+    "revision": "bb1d2724ed0ec54f019c549445c0c8ab",
+    "url": "/automotive-e-commerce-react-router/static/media/image1.bb1d2724.webp"
+  },
+  {
+    "revision": "ade993d2f9f80e264e5c902aacd203ed",
+    "url": "/automotive-e-commerce-react-router/static/media/image2.ade993d2.webp"
+  },
+  {
+    "revision": "aae7443a729806f91a707fc554cdc415",
+    "url": "/automotive-e-commerce-react-router/static/media/image3.aae7443a.webp"
+  },
+  {
+    "revision": "72879832eee2c9edf0e7b04ba2b56897",
+    "url": "/automotive-e-commerce-react-router/static/media/image4.72879832.webp"
+  },
+  {
+    "revision": "e87c15de5e49511d6167580347cfcfd8",
+    "url": "/automotive-e-commerce-react-router/index.html"
+  }
+];
